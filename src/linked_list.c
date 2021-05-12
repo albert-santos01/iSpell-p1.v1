fa6ce17572c45daaf07db6f127284942c105dc52
albert-santos01 <albert.santos01@estudiant.upf.edu>
2021-05-12 06:31:11 -0400

    Primer comtietoso outas

--- src/linked_list.c
+++ src/linked_list.c
@@ -38,27 +38,51 @@ void insert_into_list(LinkedList* l, char* c) {
 }
 
 /**
- * TODO: Iterates over the different nodes searching for a WordInfo that contains the given word.
+ * Iterates over the different nodes searching for a WordInfo that contains the given word.
  * Returns: A pointer to the node if found or NULL otherwise.
 */
 Node* find_in_list(LinkedList l, char* word) {
     // YOUR CODE HERE
+    Node* aux= l.start;
+    while (aux!=NULL){
+        if (aux->data==word){
+            return aux;
+        }
+        aux= aux->next;
+    }
+    return NULL;
 }
 
 /**
- * TODO: Given a node and the list it belongs to, it removes it from the list freeing
+ *  Given a node and the list it belongs to, it removes it from the list freeing
  * its memory. It accounts for the start, and the prev/next pointers.
 */
 void delete_node(LinkedList* l, Node* p) {
     // YOUR CODE HERE
+    Node * aux= find_in_list(*l,p->data);
+    if (aux==NULL){
+        printf("%s doesn't exists\n", p->data);
+        //Don't do nothing
+    } else{
+        aux->prev=aux->next;
+        aux->next->prev=aux->prev;
+        free(aux);
+    }
 }
 
 /**
- * TODO: If a node in the list contains a WordInfo with the given word, it deletes it.
+ *  If a node in the list contains a WordInfo with the given word, it deletes it.
  * Returns true if a node was deleted and false otherwise.
 */
 bool delete_from_list(LinkedList* l, char* word) {
     // YOUR CODE HERE
+    Node * aux= find_in_list(*l,word);
+    if (aux== NULL){
+        return false;
+    } else{
+        delete_node(l,aux);
+        return true;
+    }
 }
 
 /**
